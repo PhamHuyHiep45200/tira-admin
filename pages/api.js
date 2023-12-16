ccd7c40a6511926e49f0a9ddc40a7ac8c0de1431
thang tomosia <thangdangtomosia@gmail.com>
2023-12-17 02:33:26 +0700

    fix image

--- pages/api.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  const url = process.env.NEXT_PUBLIC_URL_LOCAL_STORAGEL; // URL của API trên EC2
-  const response = await fetch(url);
-  const data = await response.json();
-
-  res.status(200).json(data);
-}
